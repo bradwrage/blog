732928e87523c5747c9449f7f1e04a80fc25fe16
Brad Wrage <bradwrage@gmail.com>
2012-11-04 21:10:47 -0800

    added interactivity and post button

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b6b9efbfbf4857708b7f9c1ca37dd856bf7896d5ed20c81b8d74c5b448cf5403b468e26778d906290958440e3393df2a321cb267612426092711551cb14a5148'
